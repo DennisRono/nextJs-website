c4ed8084ba7a87a776289fe165c05ecd754f998a
DennisRono <finnneron1@gmail.com>
2021-04-02 15:02:24 +0300

    changes made new commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from "../styles/Footer.module.css"
 
 export const Footer = () => {
     return (
-        <div>
-            
+        <div className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.flexContainer}>
+                    <div className={styles.brands}>
+
+                    </div>
+                    <div className={styles.articles}>
+
+                    </div>
+                </div>
+                <div className={styles.copyright}>
+                    <hr/>
+                    <strong>DennisRono &copy; 2021</strong>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
+import { Footer } from '../components/Footer'
+import { Navbar } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      
+      <Navbar />
+      <Footer />
     </div>
   )
 }
